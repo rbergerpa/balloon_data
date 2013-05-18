94a97ad0fda344c29f451275ecccf379721ec34f
Bob Berger <bob@bberer.net>
2013-05-18 00:07:33 -0400

    Add plot.py

---
+++ plot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import Series, DataFrame
+from json_util import load_json
+import datetime as dt
+import sys
+
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+field = sys.argv[1]
+
+def select_time(dict):
+    return dict['time']
+
+def load_data(path):
+    json_data = list(load_json(path))
+    for d in json_data:
+        d['time'] = dt.datetime.strptime(d['time'], DATE_FORMAT)
+
+    return DataFrame(data=json_data, index=map(select_time, json_data))
+
+
+data = load_data('c1_gps.txt')
+
+data[field].plot()
+plt.show()
+
