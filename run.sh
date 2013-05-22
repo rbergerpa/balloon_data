5625558bd2d622fe291a424569f3df69120e6e69
Bob Berger <bob@bberer.net>
2013-05-22 19:45:29 -0400

    Vertical speed plots

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python plot.py hgt "Altitude (feet)"
+
+python vert_speed.py
+
+python vert_speed_hgt.py
+
+python plot.py temp3 "Temperature (Celsius)"
+
+python temp_vs_hgt.py
+
---
+++ vert_speed.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import Series, DataFrame
+from json_util import load_json
+import datetime as dt
+import sys
+
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+
+def select_time(dict):
+    return dict['time']
+
+def load_data(path):
+    json_data = list(load_json(path))
+    for d in json_data:
+        d['time'] = dt.datetime.strptime(d['time'], DATE_FORMAT)
+
+    return DataFrame(data=json_data, index=map(select_time, json_data))
+
+
+gps_data = load_data('c1_gps.txt')
+
+hgt = gps_data['hgt']
+# Delete the samples when the balloon was on the ground
+hgt = hgt[4:hgt.size-15]
+
+# delta is the height difference between samples shifted by 2 minutes
+delta = hgt.shift(-1, 'min') - hgt.shift(1, 'min')
+
+miles = delta / 5280.0
+
+mph = miles * 30  # delta shift is 30 periods per hour
+
+p = mph.plot()
+p.set_ylabel("Vertical Speed (MPH)")
+p.set_xlabel("Time")
+
+plt.show()
