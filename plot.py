a70bcfd1a0598bbcf679e40ffc668ae482060e51
Bob Berger <bob@bberer.net>
2014-03-23 02:54:12 -0400

    Add comments

--- plot.py
+++ plot.py
@@ -5,16 +5,20 @@ from pandas import Series, DataFrame
 from pandas_util import load_json_dataframe
 import sys
 
+# Convert a series indexed by time to a series indexed by altitude, 
+#     by combining the given series with the altitude vs time from gps_data
 def by_altitude(series):
     df = DataFrame({'Value': series, 'Altitude': gps_data.hgt.reindex(series.index, method='ffill')})
     return df.set_index('Altitude')['Value'].sort_index()
 
-field = sys.argv[1]
 
 gps_data = load_json_dataframe('c1_gps.txt')
 logger_data = load_json_dataframe('c1_data.txt')
 
+# Use the GPS data to determine the time of maximum altitude
 max_altitude_time = gps_data['hgt'].idxmax()
+
+field = sys.argv[1]
 
 # Set ascendeing and descending each to a Series of the specified field vs. time
 try:
