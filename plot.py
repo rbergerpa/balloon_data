04463f824c90e218388e90256f8439860ef3ca48
Bob Berger <bob@bberer.net>
2014-03-23 02:38:10 -0400

    plot.py plots by time and by altitude

--- plot.py
+++ plot.py
@@ -5,6 +5,10 @@ from pandas import Series, DataFrame
 from pandas_util import load_json_dataframe
 import sys
 
+def by_altitude(series):
+    df = DataFrame({'Value': series, 'Altitude': gps_data.hgt.reindex(series.index, method='ffill')})
+    return df.set_index('Altitude')['Value'].sort_index()
+
 field = sys.argv[1]
 
 gps_data = load_json_dataframe('c1_gps.txt')
@@ -20,15 +24,22 @@ except:
     ascending = gps_data.ix[gps_data.time <= max_altitude_time , field]
     descending = gps_data.ix[gps_data.time >= max_altitude_time, field]
 
-# Plot the ascending and descending series in different colors
-p = DataFrame({'Ascending': ascending, 'Descending': descending}).plot()
 
 if len(sys.argv) == 3:
     y_label = sys.argv[2]
 else:
     y_label = sys.argv[1]
 
+# Plot specified field vs time
+p = DataFrame({'Ascending': ascending, 'Descending': descending}).plot()
 p.set_ylabel(y_label)
 p.set_xlabel("Time")
 
+# Plot specified field vs altitude
+if field != 'hgt':
+    p = DataFrame({'Ascending': by_altitude(ascending), 'Descending': by_altitude(descending)}).plot()
+    p.set_ylabel(y_label)
+    p.set_xlabel("Altitude")
+
+# show both plots
 plt.show()
--- run.sh
+++ run.sh
@@ -2,12 +2,10 @@
 
 python plot.py hgt "Altitude (feet)"
 
+python plot.py temp3 "Temperature (Celsius)"
+
 python vert_speed.py
 
 python vert_speed_hgt.py
-
-python plot.py temp3 "Temperature (Celsius)"
-
-python temp_vs_hgt.py
 
 python speed.py
